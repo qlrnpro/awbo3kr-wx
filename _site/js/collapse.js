7abc29f10088ebc2e7c39e81149aecc34931f5b7
qlrnpro <enzystoria.id@gmail.com>
2023-09-17 15:11:37 +0700

    first commit

--- _site/js/collapse.js
+++
@@ -1,8 +0,0 @@
-// Grab HTML Elements
-const btn = document.querySelector("button.mobile-button");
-const menu = document.querySelector(".mobile-menu");
-
-// Add Event Listeners
-btn.addEventListener("click", () => {
-menu.classList.toggle("hidden");
-});
